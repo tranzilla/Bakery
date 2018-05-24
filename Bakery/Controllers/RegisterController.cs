1331c2f5e87f355a8a9b8d966043d0d5326ea3c0
Tran Duong <36751280+tranzilla@users.noreply.github.com>
2018-05-24 12:06:26 -0700

    adds NewPerson class, message class, registerController, result view, and update index with Register

---
+++ Bakery/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class RegisterController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "PersonLastName, PersonFirstName, PersonEmail, PersonPhone, PersonPlainPassword")]NewPerson np)
+        {
+            Message m = new Message();
+            int result = db.usp_newPerson(np.PersonLastName, np.PersonFirstName, np.PersonEmail, np.PersonPhone, np.PersonPlainPassword);
+
+            if (result != -1)
+            {
+                m.MessageText = "Welcome, " + np.PersonFirstName;
+
+            }
+            else
+            {
+                m.MessageText = "Sorry, but something seems to have gone wrong with the registration.";
+            }
+            return View("Result", m);
+        }
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+    }
+}
---
+++ Bakery/Models/NewPerson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bakery.Models
+{
+    public class NewPerson
+    {
+            public string PersonLastName { get; set; }
+            public string PersonFirstName { get; set; }
+            public string PersonEmail { get; set; }
+            public string PersonPhone { get; set; }
+            public string PersonPlainPassword { get; set; }
+
+
+        }
+    }
+
+
+
+
+   
