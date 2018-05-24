757649f24ec43c6243e60c8d71cc61a7216cce3d
Tran Duong <36751280+tranzilla@users.noreply.github.com>
2018-05-24 10:32:20 -0700

    Add project files.

---
+++ Bakery/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+
+namespace Bakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to D&T Bakery";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us";
+
+            return View();
+        }
+    }
+}
