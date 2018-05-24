d9ca697d7129ddf13951014c715216688ae79931
Tran Duong <36751280+tranzilla@users.noreply.github.com>
2018-05-24 11:02:17 -0700

    Add list of products

---
+++ Bakery/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class ProductController : Controller
+    {
+        // //initialized entities - making Bakery database to a class to allow data to be pass through
+        BakeryEntities db = new BakeryEntities();
+        public ActionResult Product()
+        {
+            //this will pass the collection product to the index as a list
+            return View(db.Products.ToList());
+        }
+    }
+}
