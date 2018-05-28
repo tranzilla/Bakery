b2319dfa6f4c78c835115f41475b75a03f38302c
1deewye <35506588+1deewye@users.noreply.github.com>
2018-05-28 16:49:18 -0700

    Stuff to add a working Login page

---
+++ Bakery/Controllers/LogInController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class LogInController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include="Email, Password")]LoginClass lc)
+        {
+            int results = db.usp_Login(lc.Email, lc.Password);
+            int varkey = 0;
+            Message msg = new Message();
+            if (results != -1)   
+            {
+                var pkey = (from r in db.People
+                            where r.PersonEmail.Equals(lc.Email)
+                            select r.PersonKey).FirstOrDefault();
+                varkey = (int)pkey;
+                Session["PersonKey"] = varkey;
+                msg.MessageText = "Welcome, " + lc.Email;
+
+                        
+            }
+            else
+            {
+                msg.MessageText="Invalid Login";
+            }
+            return View("result", msg);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
